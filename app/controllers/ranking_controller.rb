ef7681f1e56bf23293a681f62eeac10abd05d513
shugo kajita <kaj1m5.gon@gmail.com>
2017-03-29 06:42:25 +0000

    ranking ok

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,7 +1,11 @@
 class RankingController < ApplicationController
+
   def have
+    @items = Item.joins(:haves).group('item_id').order('count(item_id) desc').limit(10)
   end
 
   def want
+    @items = Item.joins(:wants).group('item_id').order('count(item_id) desc').limit(10)
   end
+
 end
