c785ba89af97b2505b10c6ab869b67040948a785
shugo kajita <kaj1m5.gon@gmail.com>
2017-03-29 08:06:28 +0000

    pg ver1

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,11 +1,11 @@
 class RankingController < ApplicationController
 
   def have
-    @items = Item.joins(:haves).group('item_id').order('count(item_id) desc').limit(10)
+    @items = Item.select("id", "title", "large_image").joins(:haves).group('item_id').order('count(item_id) desc').limit(10)
   end
 
   def want
-    @items = Item.joins(:wants).group('item_id').order('count(item_id) desc').limit(10)
+    @items = Item.select("id", "title", "large_image").joins(:wants).group('item_id').order('count(item_id) desc').limit(10)
   end
 
 end
